4c9b3f5d8e20fd44a15f9635700048cc9cf65023
Ulrik Danielsen <ulrik_danielsen@hotmail.com>
2020-10-08 09:45:01 +0200

    removed merge shit

--- project1.py
+++ project1.py
@@ -1,15 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-<<<<<<< HEAD
 def test_your_git(x):
     return x
 
-=======
 def gittest(x):
     x = x**2
->>>>>>> 0a30f84026e7708e03fa8854931caad403353b23
     return x**2
 
 
