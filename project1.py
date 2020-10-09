95ace134c495e48a44e21580bb9468660b4ebe89
Karen Auestad <karen.s.auestad@gmail.com>
2020-10-09 11:43:10 +0200

    kommentert litt kode

--- project1.py
+++ project1.py
@@ -8,38 +8,37 @@ class NeuralNetwork():
 
     def __init__(self, K, tau, h, y0, d, c):
         """Initialize varibles"""
-        self.K = K
-        self.tau = tau
-        self.h = h
-        self.y0 = y0
-        self.I = len(y0)
-        self.d = d
-        self.d0 = np.ndim(y0)
-        self.W = np.random.randn(self.K, self.d, self.d)
-        self.b = np.random.randn(self.K, self.d, 1)
-        self.w = np.random.randn(self.d, 1)
-        self.mu = np.random.randn(1)
-        self.c = c
-        self.Z = None
-        self.yps = None
-        self.P = None
+        self.K = K # number of layers
+        self.tau = tau # learning parameter
+        self.h = h # step length
+        self.y0 = y0 # input data
+        self.I = len(y0) # number of data points
+        self.d = d # dimension of the hidden layers
+        self.d0 = np.ndim(y0) # dimension of input data
+        self.W = np.random.randn(self.K, self.d, self.d) # weights
+        self.b = np.random.randn(self.K, self.d, 1) # bias
+        self.w = np.random.randn(self.d, 1) # parameter for the last hidden layer
+        self.mu = np.random.randn(1) # parameter for the last hidden layer
+        self.c = c # vector of given data points
+        self.Z = None # intermediate values
+        self.yps = None # vector of function values
+        self.P = None # vector of back propagation
 
-
-    def embed_1D(self):
+    def embed_1D(self): # embed input data to dimension d
         y = np.zeros(shape=(self.d, self.I))
         y[0] = self.y0
         self.y0 = y
 
-    def initialize_Z(self):
+    def initialize_Z(self): # making Z
         self.Z = np.zeros(shape=(self.K, self.d, self.I))
         self.Z[0] = self.y0
         for k in range(1, self.K):
             self.Z[k] = self.get_Z_kp1(k - 1)
 
-    def initialize_yps(self):
+    def initialize_yps(self): # making Y
         self.yps = self.hypothesis_function(np.transpose(self.Z[-1])@self.w + np.ones((self.I, 1))*self.mu)
 
-    def initialize_P(self):
+    def initialize_P(self): # making P
         self.P = np.zeros(shape=(self.K, self.d, self.I))
         self.P[-1] = self.w @ np.transpose(np.multiply((self.yps - self.c), self.hypothesis_function_derivated(np.transpose(self.Z[-1]) @ self.w + self.mu*np.ones((self.I, 1)))))
         for k in range(K - 2, -1, -1):
@@ -63,9 +62,8 @@ class NeuralNetwork():
     def transformation(self, y, k):
         return y + self.h*self.activation_function(self.W[k] @ y + self.b[k])
 
-    def get_Z_kp1(self, k):
+    def get_Z_kp1(self, k): # get Z_(k+1)
         return self.transformation(self.Z[k], k)
-
 
     def scale_y0(self):
         a = np.min(self.y0)
@@ -91,25 +89,24 @@ class NeuralNetwork():
         print("Z:", self.Z)
         print("yps:", self.yps)
 
-    def get_P_km1(self, k):
+    def get_P_km1(self, k): # get P_(k-1)
         return self.P[k + 1] + self.h*np.transpose(self.W[k]) @ (self.activation_function_derivated(self.W[k] @ self.Z[k] + self.b[k]) * self.P[k+1])
 
-
-    def dJ_dWk(self, k):
+    def dJ_dWk(self, k): # get dJ/dW_k which is element of theta
         return self.h*(self.P[k+1] * self.activation_function_derivated(self.W[k] @ self.Z[k] + self.b[k]) @ np.transpose(self.Z[k]))
 
-    def dJ_dbk(self, k):
+    def dJ_dbk(self, k): # get dJ/db_k which is element of theta
         return self.h*(self.P[k+1] * self.activation_function_derivated(self.W[k] @ self.Z[k] + self.b[k]) @ np.ones(I,1))
 
-    def dJ_dw(self, yps):
+    def dJ_dw(self, yps): # get dJ/dw which is element of theta
         Z_K = self.Z[-1]
         return Z_K @ (((yps-self.c)) * self.hypothesis_function_derivated(np.transpoose(Z_K) @ self.w + self.mu @ np.ones(I,1)))
 
-    def dJ_dmu(self, yps):
+    def dJ_dmu(self, yps): # get dJ/dmu which is element of theta
         Z_K = self.Z[-1]
         return self.hypothesis_function_derivated(np.transpose(np.transpose(Z_K) @ self.w + self.mu @ np.ones(I,1))) @ (yps-self.c)
 
-def adam_descent_step(U, dU, j, m, v):
+def adam_descent_step(U, dU, j, m, v): # One step of the adam gradient decent for one parameter
     beta_1 = 0.9
     beta_2 = 0.999
     alpha = 0.01
@@ -122,7 +119,7 @@ def adam_descent_step(U, dU, j, m, v):
     U = U - alpha*m_hat/(np.sqrt(v_hat) + eps)
     return U, m, v
 
-def simple_scheme(U_prev, dU, tau):
+def simple_scheme(U_prev, dU, tau): # One step of simple scheme to optimize weights and bias, for one parameter
     return U_prev - tau * dU
 
 
@@ -139,11 +136,11 @@ c = c.reshape((I, 1))
 network = NeuralNetwork(K, tau, h, y0, d, c)
 network.scale_input()
 network.embed_1D()
-
 network.initialize_Z()
 network.initialize_yps()
 network.initialize_P()
 print(network.P)
+
 
 
 
