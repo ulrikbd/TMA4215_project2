a094181a2531be042aaa7a5d05ec0408f615585d
Ulrik Danielsen <ulrik_danielsen@hotmail.com>
2020-10-12 16:07:39 +0200

    finished implemented training and evaluating phase with the vanilla gradient method

--- project1.py
+++ project1.py
@@ -1,18 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class NeuralNetwork():
     """
-    Neural network description
+    The Neural Network class with ResNet architecture. Contains the weights,
+    and memberfunctions to train based on input data, evaluate the solution
+    with given data, and plot the results.
     """
 
-    def __init__(self, K, tau, h, y0, d, c):
-        """Initialize varibles"""
+    def __init__(self, K, tau, h, y0, d, c, I):
+        """Initialize vaiables from the parameters"""
         self.K = K  # number of layers
         self.tau = tau  # learning parameter
         self.h = h  # step length
+        self.input = y0  #storing the initial input data
         self.y0 = y0  # input data
-        self.I = len(y0)  # number of data points
+        self.I = I  # number of data points
         self.d = d  # dimension of the hidden layers
         self.d0 = np.ndim(y0)  # dimension of input data
         self.W = np.random.randn(self.K, self.d, self.d)  # weights
@@ -21,65 +25,101 @@ class NeuralNetwork():
         self.mu = np.random.randn(1)  # parameter for the last hidden layer
         self.c = c  # vector of given data points
         
-    
+        self.scale_input()
+        self.embed()
+        
 
+    def embed(self):
+        """Embed starting values into a higher dimension"""
+        if self.d0 == 1:
+            y = np.zeros(shape=(self.d, self.I))
+            y[0] = self.y0
+            self.y0 = y
+        elif self.d0 == 2:
+            y = np.zeros(shape=(self.d, self.I))
+            y[0] = self.y0[0]
+            y[1] = self.y0[1]
+            self.y0 = y
 
-    def embed_1D(self):  # embed input data to dimension d
-        y = np.zeros(shape=(self.d, self.I))
-        y[0] = self.y0
-        self.y0 = y
-
-    def initialize_Z(self):  # making Z
+    def initialize_Z(self):  
+        """Initialize the KxdxI matrix where all the data in the
+        hidden layers are stored"""
         self.Z = np.zeros(shape=(self.K, self.d, self.I))
         self.Z[0] = self.y0
         for k in range(1, self.K):
             self.Z[k] = self.get_Z_kp1(k - 1)
 
-    def initialize_yps(self):  # making Y
+    def initialize_yps(self):
+        """Creating the Ypsilon-vector, which is the current solution"""
         self.yps = self.hypothesis_function(np.transpose(self.Z[-1]) @ self.w + np.ones((self.I, 1))*self.mu)
 
-    def initialize_P(self):  # making P
+    def initialize_P(self):
+        """Creating the KxdxI P-matrix by back propagation. These
+        values are use to calculate the gradient"""
         self.P = np.zeros(shape=(self.K, self.d, self.I))
         self.P[-1] = self.w @ np.transpose(np.multiply((self.yps - self.c), self.hypothesis_function_derivated(np.transpose(self.Z[-1]) @ self.w + self.mu*np.ones((self.I, 1)))))
         for k in range(K - 2, -1, -1):
             self.P[k] = self.get_P_km1(k)
 
     def activation_function(self, x):
+        """Non-linear scalar activation function"""
         return np.tanh(x)
 
     def activation_function_derivated(self, x):
+        """Derivative of the activation function"""
         return 1/(np.cosh(x)**2)
 
     def objective_function(self):
         return 1/2 * np.linalg.norm(self.yps - self.c)**2
 
     def hypothesis_function(self, x):
+        """Hypothesis function, could be omitted"""
         return 1/2 * (1 + np.tanh(x/2))
 
     def hypothesis_function_derivated(self, x):
+        """Derivative of the hypothesis function"""
         return 1/(2 + 2*np.cosh(x))
 
     def transformation(self, y, k):
+        """Function which maps from one layer to the next in the
+        neural networks"""
         return y + self.h*self.activation_function(self.W[k] @ y + self.b[k])
 
-    def get_Z_kp1(self, k):  # get Z_(k+1)
+    def get_Z_kp1(self, k):
+        """Supplement function which returns the value for Z at the
+        next layerss"""
         return self.transformation(self.Z[k], k)
 
+    def get_scaling_factors(self):
+        """Find the values for maxmin scaling both in the input, and in
+        the given data"""
+        self.y0_a = np.min(self.y0)
+        self.y0_b = np.max(self.y0)
+        self.c_a = np.min(self.c)
+        self.c_b = np.max(self.c)
+        
     def scale_y0(self):
-        a = np.min(self.y0)
-        b = np.max(self.y0)
-        self.y0 = (self.y0 - a) / (b - a)
+        """Scales the input values with maxmin scaling"""
+        self.y0 = (self.y0 - self.y0_a) / (self.y0_b - self.y0_a)
 
     def scale_c(self):
-        a = np.min(self.c)
-        b = np.max(self.c)
-        self.c = (self.c - a) / (b - a)
+        """Scales the given datapoints with maxmin scaling"""
+        self.c = (self.c - self.c_a) / (self.c_b - self.c_a)
 
     def scale_input(self):
+        """Scales both the input values and the given data"""
+        self.get_scaling_factors()
         self.scale_y0()
         self.scale_c()
+        
+    def scale_up_solution(self):
+        """Scales up the solution based on the factors previously found
+        for the given datapoints"""
+        self.yps = self.yps*(self.c_b - self.c_a) + self.c_a
+        
 
     def printparameters(self):
+        """Helperfunction for debugging"""
         print("K:", self.K)
         print("tau:", self.tau)
         print("d0", self.d0)
@@ -89,7 +129,8 @@ class NeuralNetwork():
         print("Z:", self.Z)
         print("yps:", self.yps)
 
-    def get_P_km1(self, k):  # get P_(k-1)
+    def get_P_km1(self, k):
+        """One step when back propagating to find P"""
         return self.P[k + 1] + self.h*np.transpose(self.W[k]) @ (self.activation_function_derivated(self.W[k] @ self.Z[k] + self.b[k]) * self.P[k+1])
 
     def dJ_dW(self): # get dJ/dW which is element of theta
@@ -114,48 +155,44 @@ class NeuralNetwork():
         return self.hypothesis_function_derivated(np.transpose(np.transpose(Z_K) @ self.w + self.mu * np.ones((I, 1))) @ (self.yps-self.c))
     
     def get_theta(self):
+        """Returns all the weights, theta, componentwise"""
         return self.dJ_dW(), self.dJ_db(), self.dJ_dw(), self.dJ_dmu()
     
     def plot_cost(self):
+        """Plots the cost function at each iteration"""
         plt.figure()
         plt.plot(np.arange(len(self.cost)), self.cost)
         plt.grid()
         plt.xlabel("Iterations")
         plt.ylabel(r'$J(\theta)$')
-    
+
             
     def train_vanilla(self, iterations):
-        self.cost = np.zeros(iterations)
+        """Training the model using the vanilla gradient method"""
+        self.cost = np.zeros(iterations)  # Initialize storage for the cost
         for i in range(iterations):
             self.initialize_Z()
             self.initialize_yps()
             self.initialize_P()
-            
+            # Get theta values
             dJ_dW, dJ_db, dJ_dw, dJ_dmu = self.get_theta()
             self.W = simple_scheme(self.W, dJ_dW, self.tau)
             self.b = simple_scheme(self.b, dJ_db, self.tau)
             self.w = simple_scheme(self.w, dJ_dw, self.tau)
             self.mu = simple_scheme(self.mu, dJ_dmu, self.tau)
             
-            self.cost[i] = self.objective_function()
-       
+            self.cost[i] = self.objective_function()        
         
     def evaluate_data(self, data):
+        """Evaluate new data with our weights found during the 
+        training phase"""
         self.y0 = data
+        self.scale_y0()
+        self.embed()
         self.initialize_Z()
         self.initialize_yps()
+        self.scale_up_solution()
         
-        
-        
-        
-    def compare(self, tol):
-        correct = 0
-        for i in range(len(self.yps)):
-            if self.yps[i] - self.c[i] < tol :
-                correct = correct + 1
-        percentage = (correct / len(self.yps))*100
-        return  correct, percentage
-
 
 def adam_descent_step(U, dU, j, m, v):  # One step of the adam gradient decent for one parameter
     beta_1 = 0.9
@@ -171,14 +208,15 @@ def adam_descent_step(U, dU, j, m, v):  # One step of the adam gradient decent f
     return U, m, v
 
 
-def simple_scheme(U, dU, tau):  # One step of simple scheme to optimize weights and bias, for one parameter
+def simple_scheme(U, dU, tau):
+    """One step of the vanilla gradient method to optimize 
+    weights and bias, for one parameter"""
     return U - tau * dU
 
 
 np.random.seed(666)
-tol = 10**(-3)
 iterations = 500
-I = 200
+I = 100
 y0 = np.random.uniform(-2, 2, I)
 K = 10
 h = 0.1
@@ -187,24 +225,50 @@ tau = 0.1
 F = lambda y: 1/2*y**2
 c = F(y0)
 c = c.reshape((I, 1))
-network = NeuralNetwork(K, tau, h, y0, d, c)
-network.scale_input()
-network.embed_1D()
+network = NeuralNetwork(K, tau, h, y0, d, c, I)
+
 network.train_vanilla(iterations)
 network.plot_cost()
+
 
 data = np.random.uniform(-2, 2, I)
 network.evaluate_data(data)
 plt.figure()
-plt.scatter(data, F(data), marker='.')
-plt.scatter(data, network.yps)
+plt.scatter(data, F(data))
+plt.scatter(data, network.yps, marker='+')
+plt.grid()
 
-plt.figure()
-x = np.linspace(-2, 2)
-plt.plot(x, (F(x) + 2) / 4)
+# 2D testing
+I = 300
+iterations = 500
+y1 = np.random.uniform(-2, 2, I)
+y2 = np.random.uniform(-2, 2, I)
+y0 = np.array([y1, y2])
+d = 4
+K = 10
+h = 0.1
+tau = 0.1
+def G(y):
+    return 1/2*(y[0]**2 + y[1]**2)
+
+c = G(y0)
+
+c = c.reshape((I, 1))
+nn = NeuralNetwork(K, tau, h, y0, d, c, I)
+nn.train_vanilla(iterations)
+nn.plot_cost()
 
 
-
+data1 = np.random.uniform(-2, 2, I)
+data2 = np.random.uniform(-2, 2, I)
+data = np.array([data1, data2])
+nn.evaluate_data(data)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(data1, data2, nn.yps)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(data1, data2, G([data1, data2]))
 
 
 
