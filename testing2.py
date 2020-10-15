00136ad7a9615a7ff829a22da414d360e4bce488
Karen Auestad <karen.s.auestad@gmail.com>
2020-10-15 18:01:53 +0200

    gjort ferdig kode til b

--- testing2.py
+++ testing2.py
@@ -11,117 +11,301 @@ plt.style.use('seaborn')
 #  other choices you need to make. Balance performance in the generalisation
 #  phase with time consumption of training.
 
-def plot(x, y, x_name, y_name, title):
-    plt.plot(x, y)
-    plt.title(title)
-    plt.xlabel(x_name)
-    plt.ylabel(y_name, rotation=0)  # turn around J(theta)
-    plt.grid(True)
-    plt.show()
+
+def test_and_train_adam(network, iterations, I):
+    """
+    For a network, train with adam and test it on new data.
+    Returns the residual of the final test on the new data points."""
+    network.train_adams_descent(iterations)
+    data = np.random.uniform(-2, 2, I)
+    network.evaluate_data(data)
+    solution = F(data)
+    solution = solution.reshape((I, 1))
+    return network.get_average_residual(solution)
 
 
-def test_K(K_vector, tau, h, y0, d, c, I):
+def test_and_train_vanilla(network, iterations, I):
+    """
+    For a network, train with vanilla and test it on new data.
+    Returns the residual of the final test on the new data points."""
+    network.vanilla(iterations)
+    data = np.random.uniform(-2, 2, I)
+    network.evaluate_data(data)
+    solution = F(data)
+    solution = solution.reshape((I, 1))
+    return network.get_average_residual(solution)
+
+
+def F(y):
+    return 1/2 * y**2
+
+
+def G(y):
+    return 1 - np.cos(y)
+
+
+"""
+SYSTEMATIC TEST
+"""
+
+
+def test_K(K_vector, tau, h, y0, d, c, I, iterations):
+    """
+    Fix all parameters but th enumer of hidden layers, K. Let K take values from a range,
+    and train and test network using the different number of hidden layers. Measure the
+    run time for each value of K."""
     cost_vec =  np.zeros(len(K_vector))
     time_vector = np.zeros(len(K_vector))
     i = 0
     for K in K_vector:
         start = timeit.default_timer()
         network = NeuralNetwork(K, tau, h, y0, d, c, I)
-        network.train_adams_descent(iterations)
-        data = np.random.uniform(-2, 2, I)
-        network.evaluate_data(data)
-        solution = F(data)
-        solution = solution.reshape((I, 1))
-        cost_vec[i] = network.get_average_residual(solution)
+        res = test_and_train_adam(network, iterations, I)
+        cost_vec[i] = res
         stop = timeit.default_timer()
         time_vector[i] = stop - start
         i = i + 1
-    plot(K_vector, cost_vec, 'number of layers, K', 'Average\nresidual', 'Average residual as function of layers')
-    plot(K_vector, time_vector, 'number of layers, K', 'runtime\n[sek]', 'Run time as function of layers')
+    return cost_vec, time_vector
 
 
-def test_tau(K, tau_vector, h, y0, d, c, I):
+def test_tau(K, tau_vector, h, y0, d, c, I, iterations):
+    """
+    Fix all parameters but the learning parameter, tau. Let tau take values from a range,
+    and train and test network using the different learning parameters. Measure the
+    run time for each value of tau."""
     cost_vec =  np.zeros(len(tau_vector))
     time_vector = np.zeros(len(tau_vector))
     i = 0
     for tau in tau_vector:
         start = timeit.default_timer()
         network = NeuralNetwork(K, tau, h, y0, d, c, I)
-        network.train_vanilla(iterations)
-        data = np.random.uniform(-2, 2, I)
-        network.evaluate_data(data)
-        solution = F(data)
-        solution = solution.reshape((I, 1))
-        cost_vec[i] = network.get_average_residual(solution)
+        res = test_and_train_adam(network, iterations, I)
+        cost_vec[i] = res
         stop = timeit.default_timer()
         time_vector[i] = stop - start
         i = i + 1
-    plot(tau_vector, cost_vec, r'$\tau$', 'Average\nresidual', r'Average residual as function of $\tau$')
-    plot(tau_vector, time_vector, 'learning parameter,' + r'$\tau$','runtime\n[sek]', r'Run time as function of $\tau$')
+    return cost_vec, time_vector
 
 
-def test_h(K, tau, h_vector, y0, d, c, I):
+def test_h(K, tau, h_vector, y0, d, c, I, iterations):
+    """
+    Fix all parameters but the step lenght, h. Let h take values from a range,
+    and train and test network using the different step lengths. Measure the
+    run time for each value of h."""
     cost_vec =  np.zeros(len(h_vector))
     time_vector = np.zeros(len(h_vector))
     i = 0
     for h in h_vector:
         start = timeit.default_timer()
         network = NeuralNetwork(K, tau, h, y0, d, c, I)
-        network.train_vanilla(iterations)
-        data = np.random.uniform(-2, 2, I)
-        network.evaluate_data(data)
-        solution = F(data)
-        solution = solution.reshape((I, 1))
-        cost_vec[i] = network.get_average_residual(solution)
+        res = test_and_train_adam(network, iterations, I)
+        cost_vec[i] = res
         stop = timeit.default_timer()
-        time_vector[i] =  stop - start
+        time_vector[i] = stop - start
         i = i + 1
-    plot(h_vector, cost_vec, 'h', 'Average\nresidual', 'Average residual as function of step length')
-    plot(h_vector, time_vector, 'step length, h', 'runtime\n[sek]', 'Run time as function of step length' )
+    return cost_vec, time_vector
 
 
-def test_d(K, tau, h, y0, d_vector, c, I):
+def test_d(K, tau, h, y0, d_vector, c, I, iterations):
+    """
+    Fix all parameters but the dimension. Let d take values from a range,
+    and train and test network using the different dimensions. Measure the
+    run time for each value of d."""
     cost_vec =  np.zeros(len(d_vector))
     time_vector = np.zeros(len(d_vector))
     i = 0
     for d in d_vector:
         start = timeit.default_timer()
         network = NeuralNetwork(K, tau, h, y0, d, c, I)
-        network.train_vanilla(iterations)
-        data = np.random.uniform(-2, 2, I)
-        network.evaluate_data(data)
-        solution = F(data)
-        solution = solution.reshape((I, 1))
-        cost_vec[i] = network.get_average_residual(solution)
+        res = test_and_train_adam(network, iterations, I)
+        cost_vec[i] = res
         stop = timeit.default_timer()
         time_vector[i] = stop - start
         i = i + 1
-    plot(d_vector, cost_vec, 'dimensions, d', 'Average\nresidual', 'Average residual as function of dimension')
-    plot(d_vector, time_vector, 'dimensions,d', 'runtime\n[sek]', 'Run time as function of dimension')
+    return cost_vec, time_vector
+
+
+def plot_K(K_vec, KF_cost, KF_time, KG_cost, KG_time):
+    """
+    Plot the result from test_K() for both F(y) and G(y). """
+    plt.plot(K_vec, KF_cost, label=r'$F(y)=\frac{1}{2}y^{2}$')
+    plt.plot(K_vec, KG_cost, label=r'$G(y)=1-\cos(y)$')
+    plt.title('Average residual as function of layers')
+    plt.xlabel('number of layers, K')
+    plt.ylabel('average residual')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    plt.plot(K_vec, KF_time, label=r'$F(y)=\frac{1}{2}y^{2}$')
+    plt.plot(K_vec, KG_time, label=r'$G(y)=1-\cos(y)$')
+    plt.title('Run time as function of layers')
+    plt.xlabel('number of layers, K')
+    plt.ylabel('runtime [sek]')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def plot_tau(tau_vec, tauF_cost, tauF_time, tauG_cost, tauG_time):
+    """
+    Plot the result from test_tau() for both F(y) and G(y). """
+    plt.plot(tau_vec, tauF_cost, label=r'$F(y)=\frac{1}{2}y^{2}$')
+    plt.plot(tau_vec, tauG_cost, label=r'$G(y)=1-\cos(y)$')
+    plt.title(r'Average residual as function of $\tau$')
+    plt.xlabel('learning parameter,' + r'$\tau$')
+    plt.ylabel('average residual')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    plt.plot(tau_vec, tauF_time, label=r'$F(y)=\frac{1}{2}y^{2}$')
+    plt.plot(tau_vec, tauG_time, label=r'$G(y)=1-\cos(y)$')
+    plt.title(r'Run time as function of $\tau$')
+    plt.xlabel('learning parameter,' + r'$\tau$')
+    plt.ylabel('runtime [sek]')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def plot_d(d_vec, dF_cost, dF_time, dG_cost, dG_time):
+    """
+    Plot the result from test_d() for both F(y) and G(y). """
+    plt.plot(d_vec, dF_cost, label=r'$F(y)=\frac{1}{2}y^{2}$')
+    plt.plot(d_vec, dG_cost, label=r'$G(y)=1-\cos(y)$')
+    plt.title('Average residual as function of dimension')
+    plt.xlabel('dimensions, d')
+    plt.ylabel('Average residual')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    plt.plot(d_vec, dF_time, label=r'$F(y)=\frac{1}{2}y^{2}$')
+    plt.plot(d_vec, dG_time, label=r'$G(y)=1-\cos(y)$')
+    plt.title('Run time as function of dimension')
+    plt.xlabel('dimensions,d')
+    plt.ylabel('runtime [sek]')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def plot_h(h_vec, hF_cost, hF_time, hG_cost, hG_time):
+    """
+    Plot the result from test_h() for both F(y) and G(y). """
+    plt.plot(h_vec, hF_cost, label=r'$F(y)=\frac{1}{2}y^{2}$')
+    plt.plot(h_vec, hG_cost, label=r'$G(y)=1-\cos(y)$')
+    plt.title(r'Average residual as function of h')
+    plt.xlabel('step length, h')
+    plt.ylabel('average residual')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    plt.plot(h_vec, hF_time, label=r'$F(y)=\frac{1}{2}y^{2}$')
+    plt.plot(h_vec, hG_time, label=r'$G(y)=1-\cos(y)$')
+    plt.title('Run time as function of step length, h')
+    plt.xlabel('step length, h')
+    plt.ylabel('runtime [sek]')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+# Testing different values for the parameters and evaluating run time
+def test_parameters():
+    """
+    Create a network and plot the results using functions above."""
+    # Fixed values
+    Iterations = 500
+    K = 12
+    h = 0.4
+    d = 3
+    tau = 0.1
+    # Varied variables
+    K_vec = np.linspace(3, 21, 19, dtype=int)
+    h_vec = np.linspace(0.01, 0.9, 10)
+    d_vec = np.linspace(1, 8, 8, dtype=int)
+    tau_vec = np.linspace(0.001, 0.9, 20)
+
+    iterations = 500
+    I = 500
+    y0 = np.random.uniform(-2, 2, I)
+    y0.sort()  # ta vekk?
+    cF = F(y0)
+    cF = cF.reshape((I, 1))
+    cG = G(y0)
+    cG = cG.reshape((I, 1))
+
+    KF_cost, KF_time = test_K(K_vec, tau, h, y0, d, cF, I, iterations)
+    KG_cost, KG_time = test_K(K_vec, tau, h, y0, d, cG, I, iterations)
+    plot_K(K_vec, KF_cost, KF_time, KG_cost, KG_time)
+
+    tauF_cost, tauF_time = test_tau(K, tau_vec, h, y0, d, cF, I, iterations)
+    tauG_cost, tauG_time = test_tau(K, tau_vec, h, y0, d, cG, I, iterations)
+    plot_tau(tau_vec, tauF_cost, tauF_time, tauG_cost, tauG_time)
+
+    hF_cost, hF_time = test_h(K, tau, h_vec, y0, d, cF, I, iterations)
+    hG_cost, hG_time = test_h(K, tau, h_vec, y0, d, cG, I, iterations)
+    plot_h(h_vec, hF_cost, hF_time, hG_cost, hG_time)
+
+    dF_cost, dF_time = test_d(K, tau, h, y0, d_vec, cF, I, iterations)
+    dG_cost, dG_time = test_d(K, tau, h, y0, d_vec, cG, I, iterations)
+    plot_d(d_vec, dF_cost, dF_time, dG_cost, dG_time)
+
+
+test_parameters()
+
+
+"""
+RANDOM TEST 
+"""
 
 
 def random_test( y0, c, I, iterations):
-    random_K = random.randint( 10, 17)
+    """
+    Generate random values for the parameters and create a neural network.
+    test and train the network and calculate the belonging average residual. """
+    random_K = random.randint(10, 17)
     random_tau = round(random.uniform( 0.01, 0.9), 2)
     random_h = round(random.uniform( 0.15, 0.5), 2)
-    random_d = random.randint( 2, 4)
-    nn = NeuralNetwork( random_K, random_tau, random_h, y0, random_d, c, I)
-    nn.train_vanilla(iterations)
-    data = np.random.uniform(-2, 2, I)
-    nn.evaluate_data(data)
-    solution = F(data)
-    solution = solution.reshape((I, 1))
-    res = nn.get_average_residual(solution)
+    random_d = random.randint(2, 4)
+    nn = NeuralNetwork(random_K, random_tau, random_h, y0, random_d, c, I)
+    res = test_and_train_adam(nn, iterations, I)
     return np.array([random_K, random_tau, random_h, random_d, res])
 
 
+def n_random_test(N):
+    """
+    Do n random test using random_test() and store the trials in the array result."""
+    iterations = 500
+    I = 500
+    y0 = np.random.uniform(-2, 2, I)
+    y0.sort() # ta vekk?
+    c = F(y0)
+    c = c.reshape((I, 1))
+    result = np.zeros((5, N))
+    for i in range(N):
+        result[:,i] = random_test(y0, c, I, iterations)
+    x = np.linspace(0,N+1,N)
+    K = result[0, :]
+    tau = result[1, :]
+    h = result[2, :]
+    d = result[3, :]
+    res = result[4, :]
+    return result
+
+
 def print_random_test(result, all, i):
+    """
+    Print either one random_test or all random tests in a array of random tests."""
     K = result[0, :]
     tau = result[1, :]
     h = result[2, :]
     res = result[4, :]
     d = result[3, :]
-    if all:
+    if all == True:
         print('K:\n', K)
         print('τ:\n', tau)
         print('h:\n', h)
@@ -131,63 +315,21 @@ def print_random_test(result, all, i):
         print('K:', K[i], '\nτ:', tau[i], '\nh:', h[i], '\nd:', d[i],'\nAverage residual:',res[i])
 
 
-# Testing different values for the parameters and evaluating run time
-# Fixed values
-K = 12
-h = 0.4
-d = 3
-tau = 0.1
-# Varied variables
-K_vec = np.linspace(3, 21, 29, dtype = int)
-h_vec = np.linspace(0.01, 0.9, 10)
-d_vec = np.linspace(1, 8, 8, dtype = int)
-tau_vec = np.linspace(0.001, 0.9, 20)
+def test_random_parameters():
+    """
+    20 random tests for the function F. The best test (lowest average residual) and the worst test
+    (highest average residual) is printed with belonging parameter values."""
+    result = n_random_test(20)
+    print_random_test(result, True, 0)
 
-iterations = 500
-I = 500
-y0 = np.random.uniform(-2, 2, I)
-y0.sort()
-F = lambda y: 1/2*y**2
-c = F(y0)
-c = c.reshape((I, 1))
+    print('\nBest')
+    i = np.where(result[4,:] == np.amin(result[4,:]))  # best
+    print_random_test(result, False, i)
 
-test_K(K_vec, tau, h, y0, d, c, I)  # [10, 17]
-# test_tau(K, tau_vec, h, y0, d, c, I)  # [0.1, 0.5]?
-# test_h(K, tau, h_vec, y0, d, c, I)  # [0.2, ->]
-# test_d(K, tau, h, y0, d_vec, c, I)  # [2, ->]
-# Ser ut til at dimmensjoner og antall lag er de som påvirker run timen mest.
-
-def N_random_test(N):
-    result = np.zeros((5, N))
-    for i in range(N):
-        #print(np.shape(random_test( y0, c, I, iterations)))
-        result[:,i] = random_test( y0, c, I, iterations)
-    x = np.linspace(0,N+1,N)
-    K = result[0, :]
-    tau = result[1, :]
-    h = result[2, :]
-    d = result[3, :]
-    res = result[4, :]
-    #plt.plot(x, K, label='K')
-    plt.plot(x, tau, label=r'$\tau$')
-    plt.plot(x, h, label='h')
-    plt.plot(x, d, label='d')
-    plt.plot(x, res, label='average residual')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-    return result
-
-#result = N_random_test(20)
-#print_random_test(result, True, 0)
-
-#print('\nBest')
-#i = np.where(result[4,:] == np.amin(result[4,:]))  # best
-#print_random_test(result, False, i)
-
-#print('\nWorst')
-#j = np.where(result[4,:] == np.amax(result[4,:]))  # worst
-#print_random_test(result, False, j)
+    print('\nWorst')
+    j = np.where(result[4,:] == np.amax(result[4,:]))  # worst
+    print_random_test(result, False, j)
 
 
+test_random_parameters()
 
